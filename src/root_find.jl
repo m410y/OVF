c6de124072feeddddbfbd60e96f0a75b3964428c
m410y <m410y@proton.me>
2024-10-02 14:34:06 +0700

    corrected root find

--- src/root_find.jl
+++ src/root_find.jl
@@ -1,29 +1,29 @@
-function dichotomy_root(f::Function, left::Real, right::Real; error=1e-5, verbose=false, max_iter=9999)
+function dichotomy_root(f::Function, left::T, right::T; error=eps(T), verbose=false, max_iter=9999) where T <: Real
     counter = 0
-    f_wrap(val) = begin
+    f_wrap(x) = begin
         counter += 1
-        f(val)
+        f(x)
     end
     retval(val) = verbose ? (val, counter) : val
     left_value = f_wrap(left)
-    left_value == 0 && return retval(left)
+    iszero(left_value) && return retval(left)
     right_value = f_wrap(right)
-    right_value == 0 && return retval(right)
+    iszero(right_value) && return retval(right)
     if sign(left_value) == sign(right_value)
         throw(ArgumentError("function xs at borders have same sign"))
     end
     if left_value > right_value
         left, right = right, left
     end
-    while abs(right_value - left_value) > error
+    while abs(right - left) > error
         middle = (left + right) / 2
-        middle_x = f_wrap(middle)
-        if middle_x < 0
+        middle_value = f_wrap(middle)
+        if middle_value < 0
             left = middle
-            left_value = middle_x
-        elseif  middle_x > 0
+            left_value = middle_value
+        elseif  middle_value > 0
             right = middle
-            right_value = middle_x
+            right_value = middle_value
         else
             return retval(middle)
         end
@@ -34,18 +34,19 @@ function dichotomy_root(f::Function, left::Real, right::Real; error=1e-5, verbos
     return retval((left + right) / 2)
 end
 
-function iteration_root(f::Function, start::Real; error=1e-5, λ=1.0, verbose=false, max_iter=9999)
+function iteration_root(f::Function, start::T; error=eps(T), λ=1.0, verbose=false, max_iter=9999) where T <: Union{Number, AbstractArray}
     counter = 0
-    f_wrap(val) = begin
+    f_wrap(x) = begin
         counter += 1
-        val - λ*f(val)
+        f(x)
     end
-    retval(val) = verbose ? (val, counter) : val
-    x = f_wrap(start)
-    x == start && return retval(start)
-    old_x, x = x, f_wrap(x)
+    retval(x) = verbose ? (x, counter) : x
+    value = f_wrap(start)
+    iszero(value) && return retval(start)
+    old_x, x = start, start - λ * value
     while abs(x - old_x) > error
-        old_x, x = x, f_wrap(x)
+        value = f_wrap(x)
+        old_x, x = x, x - λ * value
         if counter > max_iter
             throw(OverflowError("too much iterations (> $(max_iter))"))
         end
@@ -53,18 +54,39 @@ function iteration_root(f::Function, start::Real; error=1e-5, λ=1.0, verbose=fa
     return retval(x)
 end
 
-function newton_root(f::Function, J::Function, start::Real; error=1e-5, verbose=false, max_iter=9999)
+function newton_root(f::Function, J::Function, start::T; error=eps(T), verbose=false, max_iter=9999) where T <: Union{Number, AbstractArray}
     counter = 0
-    f_wrap(val) = begin
+    f_wrap(x) = begin
         counter += 1
-        val - inv(J(val))*f(val)
+        inv(J(x)) * f(x)
     end
-    retval(val) = verbose ? (val, counter) : val
-    x = f_wrap(start)
-    x == start && return retval(start)
-    old_x, x = x, f_wrap(x)
+    retval(x) = verbose ? (x, counter) : x
+    value = f_wrap(start)
+    iszero(value) && return retval(start)
+    old_x, x = start, start - value
     while abs(x - old_x) > error
-        old_x, x = x, f_wrap(x)
+        value = f_wrap(x)
+        old_x, x = x, x - value
+        if counter > max_iter
+            throw(OverflowError("too much iterations (> $(max_iter))"))
+        end
+    end
+    return retval(x)
+end
+
+function secant_root(f::Function, start::T; error=eps(T), verbose=false, λ=1.0, max_iter=9999) where T <: Union{Number, AbstractArray}
+    counter = 0
+    f_wrap(x) = begin
+        counter += 1
+        f(x)
+    end
+    retval(x) = verbose ? (x, counter) : x
+    value = f_wrap(start)
+    iszero(value) && return retval(start)
+    old_x, x = start, start - λ * value
+    while abs(x - old_x) > error
+        old_value, value = value, f_wrap(x)
+        old_x, x = x, x - (x - old_x) / (value - old_value) * value
         if counter > max_iter
             throw(OverflowError("too much iterations (> $(max_iter))"))
         end
--- tasks/task_2.jl
+++ tasks/task_2.jl
@@ -5,19 +5,9 @@ U_0 = 1.0
 a = 1.0
 
 sym_f(E) = tan(√(2(U_0 + E)) * a) - √(-E / (U_0 + E))
-sym_f_diff(E) = cos(√(2(U_0 + E)) * a)^-2 * a / 2√(2(U_0 + E)) + U_0 / 2(U_0 + E) / √(-E * (U_0 + E))
+sym_f_diff(E) = cos(√(2(U_0 + E)) * a)^-2 * a / √(2(U_0 + E)) + U_0 / 2(U_0 + E) / √(-E * (U_0 + E))
 
-println("sym energy")
 println(OVF.dichotomy_root(sym_f, -U_0 + eps(U_0), 0.0, verbose=true))
 println(OVF.iteration_root(sym_f, -U_0/2, verbose=true, λ=0.1))
 println(OVF.newton_root(sym_f, sym_f_diff, -U_0/2, verbose=true))
-
-println(OVF.dichotomy_root(OVF.j1, 1.0, 5.0, error=1e-15))
-
-# asym_f(E) = cot(√(2(U_0 + E)) * a) - √(-E / (U_0 + E))
-# asym_f_diff(E) = -sin(√(2(U_0 + E)) * a)^-2 * a / 2√(2(U_0 + E)) + U_0 / 2(U_0 + E) / √(-E * (U_0 + E))
-
-# println("asym energy")
-# println(OVF.dichotomy_root(asym_f, -U_0 + eps(U_0), 0.0, verbose=true))
-# println(OVF.iteration_root(asym_f, -U_0/2, verbose=true, λ=0.2))
-# println(OVF.newton_root(asym_f, sym_f_diff, -U_0/2, verbose=true))
+println(OVF.secant_root(sym_f, -U_0/2, verbose=true, λ=0.1))
